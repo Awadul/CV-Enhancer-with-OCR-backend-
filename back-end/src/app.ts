09171cb1f5e78c25f0742158da8ca255d79c79f0
Muhammad Awais Abdullah <awaisabdullahm@gmail.com>
2026-06-25 18:37:00 +0500

    changed the file limit to be 5mb

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -15,7 +15,8 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({ origin: '*'}));
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 app.use('/', fileRoutes);
 
