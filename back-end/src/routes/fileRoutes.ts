d0e9b72120af36ac25592b8b0cb3fab58d7c8864
Muhammad Awais Abdullah <awaisabdullahm@gmail.com>
2026-06-25 16:26:42 +0500

    fixed the downloading page issues and the padding and font size to put the maximum content on the first page

--- back-end/src/routes/fileRoutes.ts
+++ back-end/src/routes/fileRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { uploadFile } from '../controllers/fileController';
+import { generatePDFController } from '../controllers/pdfController';
 import upload from '../middleware/multerConfig';
 
 const router = Router();
 
 router.post('/file/upload', upload.single('file'), uploadFile);
+router.post('/pdf/generate', generatePDFController);
 
 export default router; 
